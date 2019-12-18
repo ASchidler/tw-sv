0e4665f498113096f297c71c4ebe7fdf8256dad3
Andre Schidler <andre@schidler.at>
2019-12-18 17:15:50 +0100

    Fixed small bug leading to miscounts at red vertex bags

--- ctw/c_sv.py
+++ ctw/c_sv.py
@@ -12,13 +12,22 @@ class CTwEncoding(sv.ImprovedSvEncoding):
         self.encode_c_counter(self.red_limit)
 
     def encode_c_counter(self, c):
+        # Add dummy-var that is always true, see below for use
+        t_var = self._add_var()
+        self._add_clause(t_var)
+
         rlist = list(self.reds)
         target_variables = [list() for _ in range(0, len(self.g.nodes))]
 
         n = 0
         for d in target_variables:
             for u in rlist:
-                d.append(self.arc[n][u])
+                # If n is red, it is in its own bag. This construct counts this. Otherwise, since arc[n][n] is always
+                # false, the red goes uncounted
+                if n == u and n in self.reds:
+                    d.append(t_var)
+                else:
+                    d.append(self.arc[n][u])
             n += 1
 
         self.encode_cardinality_sat(c, target_variables)
